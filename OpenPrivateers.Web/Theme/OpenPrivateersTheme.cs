0dddd4c37967d6091432b2665cabb3b54cc1810e
Yan <yjmrobert@gmail.com>
2023-01-26 22:30:37 -0500

    :sparkles: setup mudblazor

---
+++ OpenPrivateers.Web/Theme/OpenPrivateersTheme.cs
@@ -0,0 +1,23 @@
+﻿using MudBlazor;
+
+namespace OpenPrivateers.Web.Theme;
+
+public static class OpenPrivateersTheme
+{
+    public static MudTheme Theme = new MudTheme()
+    {
+        Typography =
+        {
+            Default =
+            {
+                FontFamily = new []
+                {
+                    "Jura",
+                    "Helvetica",
+                    "Arial",
+                    "sans-serif"
+                }
+            }
+        }
+    };
+}
