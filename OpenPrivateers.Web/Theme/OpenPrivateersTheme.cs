1cea8157a932bbcbf8d98f89a0e4c4db2e649a7c
Yan <yjmrobert@gmail.com>
2023-01-27 22:31:18 -0500

    :hammer: theme touch ups

--- OpenPrivateers.Web/Theme/OpenPrivateersTheme.cs
+++ OpenPrivateers.Web/Theme/OpenPrivateersTheme.cs
@@ -4,6 +4,8 @@ namespace OpenPrivateers.Web.Theme;
 
 public static class OpenPrivateersTheme
 {
+    public const string DarkActionHover = "#ffffff1a";
+
     public static MudTheme Theme => new()
     {
         PaletteDark = 
@@ -23,8 +25,37 @@ public static class OpenPrivateersTheme
                     "Helvetica",
                     "Arial",
                     "sans-serif"
-                }
-            }
+                },
+                FontSize = "0.7rem",
+                LineHeight = 1.125
+            },
+            H1 = new H1
+            {
+                FontSize = "3.75rem",
+                FontWeight = 300,
+                LetterSpacing = "-.00833em"
+            },
+            H2 = new H2()
+            {
+                FontSize = "2.25rem",
+                FontWeight = 400,
+            },
+            H3 = new H3()
+            {
+                FontSize = "1.5rem",
+            },
+            H4 = new H4()
+            {
+                FontSize = "1.25rem",
+            },
+            H5 = new H5()
+            {
+                FontSize = "1.125rem",
+            },
+            H6 = new H6()
+            {
+                FontSize = "1rem",
+            },
         },
         LayoutProperties =
         {
