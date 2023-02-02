19e2ce1ee0fc82ad42d590db4a998754722e8965
Yan <yjmrobert@gmail.com>
2023-02-02 13:31:17 -0500

    :wrench: added azure storage middleware

--- OpenPrivateers.Strapi/config/middlewares.js
+++ OpenPrivateers.Strapi/config/middlewares.js
@@ -1,6 +1,40 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            /**
+             * Note: If using a STORAGE_URL replace `https://${process.env.STORAGE_ACCOUNT}.blob.core.windows.net` w/ process.env.STORAGE_URL
+             * If using a CDN URL make sure to include that url in the CSP headers process.env.STORAGE_CDN_URL
+             */
+            `https://${process.env.STORAGE_ACCOUNT}.blob.core.windows.net`,
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            /**
+             * Note: If using a STORAGE_URL replace `https://${process.env.STORAGE_ACCOUNT}.blob.core.windows.net` w/ process.env.STORAGE_URL
+             * If using a CDN URL make sure to include that url in the CSP headers process.env.STORAGE_CDN_URL
+             */
+            `https://${process.env.STORAGE_ACCOUNT}.blob.core.windows.net`,
+
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
